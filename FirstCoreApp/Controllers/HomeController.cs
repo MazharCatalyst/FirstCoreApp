a166879aacfdd2568aaa4a7d24806d0d6c11b05c
Admin <Admin@DESIGNER-PC>
2019-08-23 06:49:08 -0500

    Add project files.

---
+++ FirstCoreApp/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstCoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstCoreApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public String Index() {
+            return _employeeRepository.GetEmployee(2).Name;
+        }
+
+        //public JsonResult Details()
+        //{
+        //    Employee models = _employeeRepository.GetEmployee(1);
+        //    return Json(models);
+        //}
+
+        public ViewResult Details()
+        {
+            Employee models = _employeeRepository.GetEmployee(1);
+            ViewData["PageTitle"] = "Employee Detail";
+            ViewData["Employee"] = models;
+            return View();
+        }
+
+        // For View
+        public ViewResult AllEmployee()
+        {
+           List<Employee> allemp = _employeeRepository.GetAllEmployee();
+            ViewData["AllEmployee"] = allemp;
+            return View();
+        }
+
+        // For Json Result
+        public JsonResult AllEmployeeJ()
+        {
+            List<Employee> allemp = _employeeRepository.GetAllEmployee();
+            return Json(allemp);
+        }
+
+        // For XML/Object Result
+        public ObjectResult AllEmployeeXML()
+        {
+            Employee models = _employeeRepository.GetEmployee(1);
+            return new ObjectResult(models);
+        }
+    }
+}
---
+++ FirstCoreApp/Models/MockEmployeeRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstCoreApp.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+
+        private List<Employee> _employeelist;
+
+        public MockEmployeeRepository()
+        {
+            _employeelist = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name="Employee 1", Email="Email 1", Department="IT"},
+                new Employee() { Id = 2, Name="Employee 2", Email="Email 2", Department="AC"},
+                new Employee() { Id = 3, Name="Employee 3", Email="Email 3", Department="HR"}
+
+            };
+
+        }
+        public Employee GetEmployee(int Id)
+        {
+            return _employeelist.FirstOrDefault(e => e.Id == Id);
+            //return _employeelist.Find(e => e.Id == Id);
+        }
+
+        public List<Employee> GetAllEmployee() {
+            return _employeelist;
+        }
+
+    }
+}
